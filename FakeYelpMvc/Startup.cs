6e5e8bf1bdaa81b54eaf97a326eab79a3711301c
Federico Fernandez <fede.fernandez.it@gmail.com>
2017-12-29 16:34:56 -0300

    Agregar archivos de proyecto.

---
+++ FakeYelpMvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FakeYelpMvc.Startup))]
+namespace FakeYelpMvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
